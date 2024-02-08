793b614496842ff3aac0ecb599faac22de912b50
Relampag0o <josemariar.l17@gmail.com>
2024-02-08 17:38:40 +0100

    classes added

--- models/models.py
+++ models/models.py
@@ -1,18 +1,46 @@
-# -*- coding: utf-8 -*-
+from odoo import models, fields, api
 
-# from odoo import models, fields, api
+class Supplier(models.Model):
+    _name = 'my_module.supplier'
 
+    name = fields.Char(string='Supplier Name')
+    contact_info = fields.Char(string='Contact Information')
 
-# class videogames_shop(models.Model):
-#     _name = 'videogames_shop.videogames_shop'
-#     _description = 'videogames_shop.videogames_shop'
+class Product(models.Model):
+    _name = 'my_module.product'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char(string='Game Name')
+    platform = fields.Selection([('pc', 'PC'), ('console', 'Console')], string='Platform')
+    genre = fields.Char(string='Genre')
+    available_stock = fields.Integer(string='Available Stock')
+    price = fields.Float(string='Price')
+    supplier_id = fields.Many2one('my_module.supplier', string='Supplier')
+
+class Customer(models.Model):
+    _name = 'my_module.customer'
+
+    name = fields.Char(string='Customer Name')
+    contact_info = fields.Char(string='Contact Information')
+
+class Sale(models.Model):
+    _name = 'my_module.sale'
+
+    sale_date = fields.Date(string='Sale Date')
+    sold_products = fields.Many2many('my_module.product', string='Sold Products')
+    customer_id = fields.Many2one('my_module.customer', string='Associated Customer')
+    employee_id = fields.Many2one('my_module.employee', string='Associated Employee')
+    total_sale = fields.Float(string='Total Sale')
+
+class Employee(models.Model):
+    _name = 'my_module.employee'
+
+    name = fields.Char(string='Employee Name')
+    position = fields.Char(string='Position')
+    contact_info = fields.Char(string='Contact Information')
+
+class ProductReview(models.Model):
+    _name = 'my_module.product_review'
+
+    comment = fields.Text(string='Comment')
+    rating = fields.Float(string='Rating')
+    product_id = fields.Many2one('my_module.product', string='Associated Product')
