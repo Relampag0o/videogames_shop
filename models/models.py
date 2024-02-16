59c3fe06e53f2c2b5c82911cc484fba6cdb6e55b
Relampag0o <josemariar.l17@gmail.com>
2024-02-16 10:46:32 +0100

    Update models and views for videogame products

--- models/models.py
+++ models/models.py
@@ -10,11 +10,19 @@ class Product(models.Model):
     _name = 'videogames_shop.product'
 
     name = fields.Char(string='Game Name')
+    image = fields.Binary(string='Image')
     platform = fields.Selection([('pc', 'PC'), ('console', 'Console')], string='Platform')
-    genre = fields.Char(string='Genre')
+    genre = fields.Selection([('action', 'Action'), ('adventure', 'Adventure'), ('rpg', 'RPG'), ('strategy', 'Strategy')], string='Genre')
     available_stock = fields.Integer(string='Available Stock')
     price = fields.Float(string='Price')
     supplier_id = fields.Many2one('videogames_shop.supplier', string='Supplier')
+    reserved_stock = fields.Integer(string='Reserved Stock')
+    total_stock = fields.Integer(string='Total Stock', compute='_compute_total_stock', store=True, readonly=True)
+
+    @api.depends('available_stock', 'reserved_stock')
+    def _compute_total_stock(self):
+        for record in self:
+            record.total_stock = record.available_stock - record.reserved_stock
     
    
 class Customer(models.Model):
@@ -45,3 +53,4 @@ class ProductReview(models.Model):
     comment = fields.Text(string='Comment')
     rating = fields.Float(string='Rating')
     product_id = fields.Many2one('videogames_shop.product', string='Associated Product')
+    
